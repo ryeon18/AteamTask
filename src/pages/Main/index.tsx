996f46f5624b61648710c5c18b3e0ff13c523609
Seyeon <ryeon18@naver.com>
2021-12-17 01:21:23 +0900

    Add: card hover

--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import Card from "./Card";
 import NoCard from "./NoCard";
+import dropDown from "../../assets/images/dropDown.png";
 
 const Main = () => {
   const [cardData, setCardData] = useState([]);
@@ -18,8 +19,13 @@ const Main = () => {
         <p>파트너님에게 딱 맞는 요청서를 찾아보세요</p>
       </section>
       <section className="filterBox">
-        <button className="methodButton">가공방식</button>
-        <button className="materialButton">재료</button>
+        <button className="methodButton">
+          가공방식
+          <img alt="dropDownIcon" src={dropDown} />
+        </button>
+        <button className="materialButton">
+          재료 <img alt="dropDownIcon" src={dropDown} />
+        </button>
 
         <button className="toggle">토글</button>
         <span>상담 중인 요청만 보기</span>
