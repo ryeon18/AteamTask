3a69a069e5478aa0b6bfae5d1502ac91d16df8a2
Seyeon <ryeon18@naver.com>
2021-12-17 15:42:32 +0900

    Adding: mobile layout

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,11 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import logo from "../assets/images/logo.png";
+import colorlogo from "../assets/images/colorlogo.png";
+import mobileVector from "../assets/images/mobileVector.png";
 import vector from "../assets/images/vector.png";
 
 const Nav: React.FC = () => {
+  const [modal, setModal] = useState(false);
   return (
     <header className="navBox">
       <div className="logoBox">
+        <div onClick={() => setModal(!modal)}>
+          <i className="fas fa-bars" />
+        </div>
+        <div className={`${modal ? "showModal" : "non-modal"}`}>
+          <div className="showModal" onClick={() => setModal(!modal)}>
+            <div className="modal">
+              <img className="logo" alt="logo" src={colorlogo} />
+              <ul className="modalUserBox">
+                <li>
+                  <img
+                    className="vectorImage"
+                    alt="vector"
+                    src={mobileVector}
+                  />
+                </li>
+                <li>파트너정밀가공</li>
+                <li>로그아웃</li>
+              </ul>
+            </div>
+          </div>
+        </div>
         <img className="logo" alt="logo" src={logo} />
         <ul className="userBox">
           <li>
--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -5,12 +5,24 @@ import dropDown from "../../assets/images/dropDown.png";
 
 const Main = () => {
   const [cardData, setCardData] = useState([]);
+  const [checkSwitch, setCheckSwitch] = useState(false);
 
   useEffect(() => {
     fetch("/Carddata.json")
       .then((res) => res.json())
       .then((cardData) => setCardData(cardData.requests));
   }, []);
+
+  function showConsultant(cardData: any) {
+    const getConsultant = cardData.filter(
+      (cardData: any) => cardData.status === "상담중"
+    );
+
+    return console.log("data", getConsultant);
+  }
+
+  // const deletedCommentList = commentBox.filter(item => item.id !== id);
+  // this.setState({ commentBox: deletedCommentList });
 
   return (
     <main className="mainpage">
@@ -19,18 +31,22 @@ const Main = () => {
         <p>파트너님에게 딱 맞는 요청서를 찾아보세요</p>
       </section>
       <section className="filterBox">
-        <button className="methodButton">
-          가공방식
-          <img alt="dropDownIcon" src={dropDown} />
-        </button>
-        <button className="materialButton">
-          재료 <img alt="dropDownIcon" src={dropDown} />
-        </button>
-
-        <button className="toggle">토글</button>
-        <span>상담 중인 요청만 보기</span>
+        <div className="buttonBox">
+          <button className="methodButton">
+            가공방식
+            <img alt="dropDownIcon" src={dropDown} />
+          </button>
+          <button className="materialButton">
+            재료 <img alt="dropDownIcon" src={dropDown} />
+          </button>
+        </div>
+        <div className="toggleBox">
+          <button className="toggle" onClick={() => showConsultant(cardData)}>
+            토글
+          </button>
+          <span>상담 중인 요청만 보기</span>
+        </div>
       </section>
-      {/* <Card2 /> */}
       {/* <NoCard /> */}
       {cardData.length > 0 ? <Card item={cardData} /> : <NoCard />}
     </main>
